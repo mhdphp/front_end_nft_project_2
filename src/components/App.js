3a2211b267a6d0d890d85877fe38a5e8ee6bf089
Mihai Enuica <mihai.enuica@gmail.com>
2022-03-21 11:16:40 +0200

    Checking the mint function Front-End

--- src/components/App.js
+++ src/components/App.js
@@ -116,6 +116,7 @@ class App extends Component {
     render() {
         return(
             <div>
+                {console.log(this.state.kBirdz)}
                 <nav className='navbar navbar-dark fix-top bg-dark flex-md-nownap p-0 shadow'>
                     <div className='navbar-brand col-sm-3 col-md-3 mr-0' style={{font:'white'}}>
                         KryptoBirdz NFTs (Non Fungible Tokens)
